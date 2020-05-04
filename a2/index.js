905b01a5c99caf2c1a7821458e912d103115cf0b
Minh Dang <d.baminh@gmail.com>
2020-05-04 21:41:50 +0700

    Refactor code

--- a2/index.js
+++ a2/index.js
@@ -1,7 +1,74 @@
+const movies = [
+  {
+    title: "Avengers: Endgame",
+    rating: "PG",
+    plot:
+      "After the devastating events of Avengers: Infinity War (2018), the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to reverse Thanos' actions and restore balance to the universe.",
+    schedule: [
+      "Mon - N/A",
+      "Tue - N/A",
+      "Wed - 9:00 p.m",
+      "Thu - 9:00 p.m",
+      "Fri - 9:00 p.m",
+      "Sat - 6:00 p.m",
+      "Sun - 6:00 p.m"
+    ],
+    trailer: "https://www.youtube.com/embed/TcMBFSGVi1c"
+  },
+  {
+    title: "Top End Wedding",
+    rating: "M",
+    plot:
+      "Lauren and Ned are engaged, they are in love, and they have just ten days to find Lauren's mother who has gone AWOL somewhere in the remote far north of Australia, reunite her parents and pull off their dream wedding.",
+    schedule: [
+      "Mon - 6:00 p.m",
+      "Tue - 6:00 p.m",
+      "Wed - N/A",
+      "Thu - N/A",
+      "Fri - N/A",
+      "Sat - 3:00 p.m",
+      "Sun - 3:00 p.m"
+    ],
+    trailer: "https://www.youtube.com/embed/uoDBvGF9pPU"
+  },
+  {
+    title: "Dumbo",
+    rating: "PG",
+    plot:
+      "A young elephant, whose oversized ears enable him to fly, helps save a struggling circus, but when the circus plans a new venture, Dumbo and his friends discover dark secrets beneath its shiny veneer.",
+    schedule: [
+      "Mon - 12:00 p.m",
+      "Tue - 12:00 p.m",
+      "Wed - 6:00 p.m",
+      "Thu - 6:00 p.m",
+      "Fri - 6:00 p.m",
+      "Sat - 12:00 p.m",
+      "Sun - 12:00 p.m"
+    ],
+    trailer: "https://www.youtube.com/embed/7NiYVoqBt-8"
+  },
+  {
+    title: "The Happy Prince",
+    rating: "R",
+    plot:
+      "The untold story of the last days in the tragic times of Oscar Wilde, a person who observes his own failure with ironic distance and regards the difficulties that beset his life with detachment and humor.",
+    schedule: [
+      "Mon - N/A",
+      "Tue - N/A",
+      "Wed - 12:00 p.m",
+      "Thu - 12:00 p.m",
+      "Fri - 12:00 p.m",
+      "Sat - 9:00 p.m",
+      "Sun - 9:00 p.m"
+    ],
+    trailer: "https://www.youtube.com/embed/tXANCJQkUIE"
+  }
+];
+
 // Change the navigation link's appearance based on the section
 // When click on the link
-let addCurrentLinkEffect = link => {
-  let currentLink = document.getElementsByClassName("current_link");
+const addCurrentLinkEffect = link => {
+  const currentLink = document.getElementsByClassName("current_link");
   currentLink[0].classList.remove("current_link");
   link.classList.add("current_link");
 };
@@ -10,142 +77,76 @@ let addCurrentLinkEffect = link => {
 window.onscroll = () => {
   const navbar = document.getElementById("navbar");
   const aboutSection = document.getElementById("about-us-section");
-  const priceSection = document.getElementById("price-section");
-  const nowShowingSection = document.getElementById("now-showing-section");
+  const priceSection = document.getElementById("price");
+  const nowShowingSection = document.getElementById("now-showing");
   if (
     window.scrollY + navbar.offsetHeight >= aboutSection.offsetTop &&
     window.scrollY + navbar.offsetHeight < priceSection.offsetTop
   ) {
-    let link = document.getElementById("about-us_link");
+    const link = document.getElementById("about-us_link");
     addCurrentLinkEffect(link);
     //console.log("about us section");
   } else if (
     window.scrollY + navbar.offsetHeight >= priceSection.offsetTop &&
     window.scrollY + navbar.offsetHeight < nowShowingSection.offsetTop
   ) {
-    let link = document.getElementById("price_link");
+    const link = document.getElementById("price_link");
     addCurrentLinkEffect(link);
     //console.log("price section");
   } else if (
     window.scrollY + navbar.offsetHeight >=
     nowShowingSection.offsetTop
   ) {
-    let link = document.getElementById("now-showing_link");
+    const link = document.getElementById("now-showing_link");
     addCurrentLinkEffect(link);
     //console.log("now showing section");
   } else {
-    let link = document.getElementById("home_link");
+    const link = document.getElementById("home_link");
     addCurrentLinkEffect(link);
     //console.log("Home");
   }
 };
 
-let changeScreeningDate = (schedule, screeningTimes) => {
-    for (i = 0; i < schedule.length; i++) {
-        [date, time] = schedule[i].innerText.split("-");
-        schedule[i].innerText = date + "- " + screeningTimes[i];
-    }
+panelOnClickHandler = (e, movieIndex) => {
+  addActivePanelEffect(e);
+  updateSynopsis(movieIndex);
+  
+  scrollToSynopsisArea();
 };
 
-let updateSynopsis = panel => {
-  // Get the content from the clicked panel
-  let title = panel.getElementsByClassName("panel__content__title")[0]
-    .innerText;
-  let rating = panel.getElementsByClassName("panel__content__rating")[0]
-    .innerText;
-  let content = panel.getElementsByClassName("panel__content__rating")[0]
-    .innerText;
-
-  // Get the synopsis's components needed to be updated
-  let synopsis_title = document.getElementById("synopsis__content__title");
-  let synopsis_rating = document.getElementById("synopsis__content__rating");
-  let synopsis_movie_desc = document
-    .getElementById("synopsis__content__plot")
-    .getElementsByTagName("p")[0];
-  let synopsis_video_link = document.getElementById("synopsis__trailer__video");
-  let schedule = document.getElementsByClassName(
-    "synopsis__booking-bar__button"
-  );
-  // Update the synopsis's components
-  synopsis_title.innerText = title;
-  synopsis_rating.innerText = rating;
-
-  switch (title) {
-    case "Avengers: Endgame".toUpperCase():
-      synopsis_movie_desc.innerText =
-        "After the devastating events of Avengers: Infinity War (2018)," +
-        " the universe is in ruins. With the help of remaining allies, the Avengers assemble" +
-        " once more in order to reverse Thanos' actions and restore balance to the universe.";
-      synopsis_video_link.src = "https://www.youtube.com/embed/TcMBFSGVi1c";
-      changeScreeningDate(schedule, [
-        "N/A",
-        "N/A",
-        "9:00 p.m",
-        "9:00 p.m",
-        "9:00 p.m",
-        "6:00 p.m",
-        "6:00 p.m"
-      ]);
-      break;
-
-    case "Top End Wedding".toUpperCase():
-      synopsis_movie_desc.innerText =
-        "From the makers of The Sapphires, TOP END WEDDING is a celebration of love," +
-        " family and belonging, set against the spectacular natural beauty of the Northern Territory. This heartwarming" +
-        " romantic comedy tells the story of successful Adelaide lawyer Lauren (Tapsell) and her fiancé Ned (Lee)." +
-        " Engaged and in love, they have just ten days to pull off their dream Top End Wedding.";
-
-      synopsis_video_link.src = "https://www.youtube.com/embed/uoDBvGF9pPU";
-
-      changeScreeningDate(schedule, [
-        "6:00 p.m",
-        "6:00 p.m",
-        "N/A",
-        "N/A",
-        "N/A",
-        "3:00 p.m",
-        "3:00 p.m"
-      ]);
-      break;
-
-    case "Dumbo".toUpperCase():
-      synopsis_movie_desc.innerText =
-        "Circus owner Max Medici (Danny DeVito) enlists former star Holt Farrier (Colin Farrell)" +
-        " and his children Milly (Nico Parker) and Joe (Finley Hobbins) to care for a newborn elephant whose oversized ears" +
-        " make him a laughingstock in an already struggling circus. But when they discover that Dumbo can fly, the circus" +
-        " makes an incredible comeback, attracting persuasive entrepreneur V.A.";
-      synopsis_video_link.src = "https://www.youtube.com/embed/7NiYVoqBt-8";
-      changeScreeningDate(schedule, [
-        "12:00 p.m",
-        "12:00 p.m",
-        "6:00 p.m",
-        "6:00 p.m",
-        "6:00 p.m",
-        "12:00 p.m",
-        "12:00 p.m"
-      ]);
-      break;
-
-    case "The Happy Prince".toUpperCase():
-      synopsis_movie_desc.innerText =
-        "In a cheap Parisian hotel room Oscar Wilde lies on his death bed and the past floods back," +
-        " transporting him to other times and places. Was he once the most famous man in London? The artist crucified by a" +
-        " society that once worshiped him? The lover imprisoned and freed, yet still running towards ruin in the final chapter" +
-        " of his life?";
-      synopsis_video_link.src = "https://www.youtube.com/embed/tXANCJQkUIE";
-      changeScreeningDate(schedule, [
-        "N/A",
-        "N/A",
-        "12:00 p.m",
-        "12:00 p.m",
-        "12:00 p.m",
-        "9:00 p.m",
-        "9:00 p.m"
-      ]);
-      break;
+addActivePanelEffect = e => {
+  // update "active" status on panels
+  const active_panel = document.getElementsByClassName("panel active");
+  if (active_panel.length != 0) {
+    active_panel[0].classList.remove("active");
   }
-  // EXTRA WORK:
-  // Scroll to the synopsis when user click on any panel
+  e.classList.add("active");
+};
+
+updateSynopsis = movieIndex => {
+  // get elements of synopsis area
+  const title = document.getElementById("synopsis__content__title");
+  const rating = document.getElementById("synopsis__content__rating");
+  const plot = document.getElementById("synopsis__content__plot");
+  const bookingContainer = document.getElementById("booking-button__container");
+  const trailer = document.getElementById("synopsis__trailer__video");
+
+  // update content of those elements
+  title.innerHTML = movies[movieIndex].title;
+  rating.innerHTML = movies[movieIndex].rating;
+  plot.innerHTML = `<h2>Plot description:</h2>
+                        <p>${movies[movieIndex].plot}</p>`;
+
+  bookingButtons = Array.from(bookingContainer.children);
+  for (i = 0; i < 7; i++) {
+    bookingButtons[i].innerHTML = movies[movieIndex].schedule[i];
+  }
+  trailer.src = movies[movieIndex].trailer;
+};
+
+scrollToSynopsisArea = () => {
+  // scroll into synopsis area
   let synopsis = document.getElementById("synopsis");
+  synopsis.classList.remove("invisible"); // remove invisible when the page first loaded
   synopsis.scrollIntoView();
 };
